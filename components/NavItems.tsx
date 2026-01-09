b3117096d58fa5beb8fd911d46a33d9522c6a104
chanmin0311 <ckstj1500@gmail.com>
2026-01-09 20:43:15 +0900

    feat(component): Implement navigation bar

---
+++ components/NavItems.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navItems = [
+    { label: "Home", href: "/" },
+    { label: "Companions", href: "/companions" },
+    { label: "My Journey", href: "/my-journey" },
+];
+
+const NavItems = () => {
+    const path = usePathname();
+    console.log(path);
+    return (
+        <nav className="flex items-center gap-4">
+            {navItems.map(({ label, href }) => (
+                <Link
+                    href={href}
+                    key={label}
+                    className={cn(
+                        path === href && "text-primary font-semibold"
+                    )}
+                >
+                    {label}
+                </Link>
+            ))}
+        </nav>
+    );
+};
+
+export default NavItems;
