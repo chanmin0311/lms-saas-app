078ee4f05da83df987449f3f95b4567175b52a05
chanmin0311 <ckstj1500@gmail.com>
2026-01-11 23:33:44 +0900

    feat: add updated auth and companion UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,48 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import { recentSessions } from "@/constants";
+import CTA from "@/components/CTA";
 
 const Page = () => {
     return (
-        <div>
-            <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-            <Button>Let's get our hands dirty</Button>
-        </div>
+        <main>
+            <h1>Popular Companions</h1>
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura the Binaray Explorer"
+                    topic="Neural Network of the Brain"
+                    subject="science"
+                    duration={45}
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Countsy the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    subject="math"
+                    duration={30}
+                    color="#e5d0ff"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Verba the Vocabularay Builder"
+                    topic="language"
+                    subject="English Literature"
+                    duration={30}
+                    color="#bde7ff"
+                />
+            </section>
+
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessions}
+                    className="w-2/3 max-lg:w-full"
+                />
+                <CTA />
+            </section>
+        </main>
     );
 };
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/images/logo.svg";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
@@ -13,7 +14,14 @@ const Navbar = () => {
             </Link>
             <div className="flex items-center gap-8">
                 <NavItems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     );
